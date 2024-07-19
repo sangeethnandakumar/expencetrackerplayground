e0679108c98f63622bdb345e10052a3fae42d63a
Sangeeth Nandakumar <sangeethnandakumar@gmail.com>
2024-07-20 01:20:53 +0530

    fix - UI updates

---
+++ expencetrackerplayground/src/Api.js
@@ -0,0 +1,135 @@
+import axios from 'axios';
+
+const baseURL = import.meta.env.VITE_APP_API_URL;
+const scopes = [];
+
+const axiosInstance = axios.create({
+    baseURL: baseURL,
+});
+
+const tryRelogin = msalInstance => {
+    if (!msalInstance) return;
+    msalInstance.logoutPopup();
+    setTimeout(() => msalInstance.loginPopup(), 2000);
+}
+
+const fetchMSALToken = async msalInstance => {
+    if (!msalInstance) return null;
+    try {
+        const token = await msalInstance.acquireTokenSilent({
+            scopes: scopes,
+        });
+        return token ? token.accessToken : null;
+    } catch (error) {
+        if (msalInstance) {
+            tryRelogin(msalInstance);
+        }
+        throw error;
+    }
+}
+
+const fetchLocalToken = () => window.localStorage.getItem('token');
+
+const handleResponse = (response, callbacks) => {
+    const { onSuccess, onError, onBadRequest, onForbid, onUnauthorized } = callbacks;
+
+    if (response.status === 200 && onSuccess) {
+        onSuccess(response.data);
+    } else if (response.status === 400 && onBadRequest) {
+        onBadRequest(response.data);
+    } else if (response.status === 401) {
+        onUnauthorized && onUnauthorized(response.data);
+    } else if (response.status === 403 && onForbid) {
+        onForbid(response.data);
+    } else if (onError) {
+        onError(response.data);
+    }
+}
+
+const axiosRequest = async (method, url, headers, data = null, params = null) => {
+    try {
+        let response;
+        switch (method) {
+            case 'GET':
+                response = await axiosInstance.get(url, { headers, params });
+                break;
+            case 'POST':
+                response = await axiosInstance.post(url, data, { headers });
+                break;
+            case 'PUT':
+                response = await axiosInstance.put(url, data, { headers });
+                break;
+            case 'PATCH':
+                response = await axiosInstance.patch(url, data, { headers });
+                break;
+            case 'DELETE':
+                response = await axiosInstance.delete(url, { headers, params });
+                break;
+            default:
+                throw new Error(`Unsupported method: ${method}`);
+        }
+        return response;
+    } catch (error) {
+        console.error(error);
+        return error.response;
+    }
+}
+
+const makeRequest = async (method, url, data, callbacks, msalInstance = null) => {
+    try {
+        const token = msalInstance ? await fetchMSALToken(msalInstance) : fetchLocalToken();
+        const headers = {
+            'Authorization': `Bearer ${token}`,
+            'Content-Type': 'application/json'
+        };
+
+        const response = await axiosRequest(method, url, headers, data);
+
+        handleResponse(response, callbacks);
+    } catch (error) {
+        console.warn(error);
+        handleResponse(error.response, callbacks);
+    }
+}
+
+const uploadFile = async (url, file, callbacks, msalInstance = null) => {
+    try {
+        const token = msalInstance ? await fetchMSALToken(msalInstance) : fetchLocalToken();
+        const headers = {
+            'Authorization': `Bearer ${token}`,
+        };
+
+        const formData = new FormData();
+        formData.append('files', file);
+
+        const config = {
+            headers: { ...headers, 'Content-Type': 'multipart/form-data' },
+            onUploadProgress: (progressEvent) => {
+                const percentCompleted = Math.round((progressEvent.loaded * 100) / progressEvent.total);
+                callbacks.onUpload && callbacks.onUpload(percentCompleted);
+            },
+        };
+
+        const response = await axiosInstance.post(url, formData, config);
+
+        if (response.status === 200) {
+            callbacks.onSuccessfulUpload && callbacks.onSuccessfulUpload(response.data);
+        } else {
+            callbacks.onUploadFailure && callbacks.onUploadFailure(response.data);
+        }
+    } catch (error) {
+        console.warn(error);
+        callbacks.onUploadFailure && callbacks.onUploadFailure(error.response.data);
+    }
+};
+
+const Api = {
+    GET: async (url, params, callbacks, msalInstance = null) => await makeRequest('GET', url, params, callbacks, msalInstance),
+    POST: async (url, data, callbacks, msalInstance = null) => await makeRequest('POST', url, data, callbacks, msalInstance),
+    PUT: async (url, data, callbacks, msalInstance = null) => await makeRequest('PUT', url, data, callbacks, msalInstance),
+    PATCH: async (url, data, callbacks, msalInstance = null) => await makeRequest('PATCH', url, data, callbacks, msalInstance),
+    DELETE: async (url, callbacks, msalInstance = null) => await makeRequest('DELETE', url, null, callbacks, msalInstance),
+    UPLOAD: async (url, file, callbacks, msalInstance = null) => await uploadFile(url, file, callbacks, msalInstance),
+};
+
+export default Api;
--- expencetrackerplayground/src/App.jsx
+++ expencetrackerplayground/src/App.jsx
@@ -1,35 +1,237 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Divider, Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react'
+import Api from './Api';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import { Card, CardHeader, CardBody, Heading, Stack, StackDivider, Box, Text } from '@chakra-ui/react'
+import {
+    FormControl,
+    FormLabel,
+    FormErrorMessage,
+    FormHelperText,
+    Input
+} from '@chakra-ui/react'
+import { Select } from '@chakra-ui/react'
+import { Button, ButtonGroup } from '@chakra-ui/react'
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    const [tracks, setTracks] = useState([]);
+    const [catageories, setCatageories] = useState([]);
+
+    const [date, setDate] = useState('');
+    const [exp, setExp] = useState(0);
+    const [notes, setNotes] = useState('Notes');
+    const [catageory, setCatageory] = useState('');
+
+    function loadTracks() {
+        Api.GET(
+            'https://expensetracker.twileloop.com/tracks',
+            null,
+            {
+                onSuccess: (data) => {
+                    setTracks(data);
+                    console.log('GET request successful:', data);
+                },
+                onError: (error) => {
+                    console.error('Error in GET request:', error);
+                },
+                onBadRequest: (data) => {
+                    console.error('Bad request in GET:', data);
+                },
+                onUnauthorized: (data) => {
+                    console.error('Unauthorized in GET:', data);
+                },
+                onForbid: (data) => {
+                    console.error('Forbidden in GET:', data);
+                }
+            }, null
+        );
+    }
+
+    function loadCatageories() {
+        Api.GET(
+            'https://expensetracker.twileloop.com/categories',
+            null,
+            {
+                onSuccess: (data) => {
+                    setCatageories(data);
+                    console.log('GET request successful:', data);
+                },
+                onError: (error) => {
+                    console.error('Error in GET request:', error);
+                },
+                onBadRequest: (data) => {
+                    console.error('Bad request in GET:', data);
+                },
+                onUnauthorized: (data) => {
+                    console.error('Unauthorized in GET:', data);
+                },
+                onForbid: (data) => {
+                    console.error('Forbidden in GET:', data);
+                }
+            }, null
+        );
+    }
+
+    function addTrack() {
+        const postData = {
+            date: date,
+            exp: exp,
+            inc: 0,
+            notes: notes,
+            category: catageory
+        };
+        Api.POST(
+            'https://expensetracker.twileloop.com/tracks',
+            postData,
+            {
+                onSuccess: (data) => {
+                    console.log('POST request successful:', data);
+                    loadTracks();
+                    loadCatageories();
+                },
+                onError: (error) => {
+                    console.error('Error in POST request:', error);
+                },
+                onBadRequest: (data) => {
+                    console.error('Bad request in POST:', data);
+                },
+                onUnauthorized: (data) => {
+                    console.error('Unauthorized in POST:', data);
+                },
+                onForbid: (data) => {
+                    console.error('Forbidden in POST:', data);
+                }
+            },
+            null
+        );
+    }
+
+    function formatDateToISO() {
+        const date = new Date();
+
+        const pad = (number, length = 2) => {
+            return number.toString().padStart(length, '0');
+        };
+
+        const year = date.getUTCFullYear();
+        const month = pad(date.getUTCMonth() + 1); // Months are zero-based
+        const day = pad(date.getUTCDate());
+        const hours = pad(date.getUTCHours());
+        const minutes = pad(date.getUTCMinutes());
+        const seconds = pad(date.getUTCSeconds());
+        const milliseconds = pad(date.getUTCMilliseconds(), 3); // Pad to 3 digits
+
+        return `${year}-${month}-${day}T${hours}:${minutes}:${seconds}.${milliseconds}Z`;
+    }
+
+    console.log(formatDateToISO());
+
+
+
+    useEffect(x => {
+        loadCatageories();
+        loadTracks();
+        setDate(formatDateToISO());
+    }, []);
+
+    return (
+        <>
+            <Tabs variant='soft-rounded' colorScheme='green'>
+                <TabList>
+                    <Tab onClick={() => loadCatageories() }>Catageories</Tab>
+                    <Tab>Traces</Tab>
+                    <Tab>Report</Tab>
+                </TabList>
+                <TabPanels>
+                    <TabPanel>
+
+                        <Card>
+                            <CardHeader>
+                                <Heading size='md'>All Catageories</Heading>
+                            </CardHeader>
+
+                            <CardBody>
+                                <Stack divider={<StackDivider />} spacing='4'>
+                                    {catageories.map(x => {
+                                        return (
+                                            <>
+                                                <Heading size='xs' textTransform='uppercase'>
+                                                    {x.title}
+                                                </Heading>
+                                                <Text pt='2' fontSize='sm'>
+                                                    {x.text}
+                                                </Text>
+                                            </>
+                                        );
+                                    })}
+                                    <Box>
+
+                                    </Box>
+                                </Stack>
+                            </CardBody>
+                        </Card>
+
+
+                    </TabPanel>
+                    <TabPanel>
+                        <FormControl>
+                            <FormLabel>Date</FormLabel>
+                            <Input type='text' value={date} />
+
+                            <FormLabel>Expence</FormLabel>
+                            <Input type='number' value={exp} onChange={(e) => setExp(e.target.value)} />
+
+                            <FormLabel>Notes</FormLabel>
+                            <Input type='text' value={notes} onChange={(e) => setNotes(e.target.value)} />
+
+                            <FormLabel>Catageory</FormLabel>
+                            <Select onChange={(e) => setCatageory(e.target.value)} >
+                                {catageories.map(x => {
+                                    return <option key={x.id} value={x.id} >{x.title}</option>;
+                                })}
+                            </Select>
+
+                        </FormControl>
+
+                        <Button colorScheme='blue' onClick={() => addTrack()}>Add Track</Button>
+
+
+
+                        <Card>
+                            <CardHeader>
+                                <Heading size='md'>All Tracks</Heading>
+                            </CardHeader>
+
+                            <CardBody>
+                                <Stack divider={<StackDivider />} spacing='4'>
+                                    {tracks.map(x => {
+                                        return (
+                                            <>
+                                                <Heading size='xs' textTransform='uppercase'>
+                                                    {x.notes}
+                                                </Heading>
+                                                <Text pt='2' fontSize='sm'>
+                                                    INR {x.exp} /-
+                                                </Text>
+                                                <Text pt='2' fontSize='md'>
+                                                    {catageories.find(y => y.id == x.category)?.title}
+                                                </Text>
+                                            </>
+                                        );
+                                    })}
+                                    <Box>
+
+                                    </Box>
+                                </Stack>
+                            </CardBody>
+                        </Card>
+
+                    </TabPanel>
+                </TabPanels>
+            </Tabs>
+        </>
+    )
 }
 
 export default App
